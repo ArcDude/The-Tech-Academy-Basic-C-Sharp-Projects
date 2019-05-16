60e9169fc28f335aa4e01ed99bd739fe07e5b5b0
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-05-16 13:47:16 -0500

    TechDailyReport WIP

---
+++ TechDailyReport/TechDailyReport/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechDailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+
+        }
+    }
+}
