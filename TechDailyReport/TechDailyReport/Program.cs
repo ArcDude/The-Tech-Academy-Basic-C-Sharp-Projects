a9546122625c6e35d33e21fb111f64ed251a322a
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-05-16 14:35:10 -0500

    Update Program.cs

--- TechDailyReport/TechDailyReport/Program.cs
+++ TechDailyReport/TechDailyReport/Program.cs
@@ -13,7 +13,13 @@ namespace TechDailyReport
             Console.WriteLine("The Tech Academy");
             Console.WriteLine("Student Daily Report");
             Console.WriteLine("What course are you on?");
-
+            string courseName = Console.ReadLine();
+            Console.WriteLine("I am on the " + courseName + " course");
+            Console.WriteLine("What page number?");
+            string pageNum = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(pageNum);
+            Console.WriteLine("I am on page number " + pageNum);
+            Console.ReadLine();
         }
     }
 }
