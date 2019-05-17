7e6449ea3a5d3423ea89b198feced9ef02896867
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-05-17 13:59:32 -0500

    Update Program.cs

--- TechDailyReport/TechDailyReport/Program.cs
+++ TechDailyReport/TechDailyReport/Program.cs
@@ -16,9 +16,22 @@ namespace TechDailyReport
             string courseName = Console.ReadLine();
             Console.WriteLine("I am on the " + courseName + " course");
             Console.WriteLine("What page number?");
-            string pageNum = Console.ReadLine();
-            int pageNumber = Convert.ToInt32(pageNum);
+            string pageNumber = Console.ReadLine();
+            int pageNum = Convert.ToInt32(pageNumber);
             Console.WriteLine("I am on page number " + pageNum);
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string needHelp = Console.ReadLine();
+            bool help = Convert.ToBoolean(needHelp);
+            Console.ReadLine();
+            Console.WriteLine("Were there any positive experiances you'd like to share? Please give specifics.");
+            string positive = Console.ReadLine();
+            Console.WriteLine("Is there any of feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            int hrs = Convert.ToInt32(hours);
+            Console.WriteLine("I studdied " + hrs + " today");
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
         }
     }
