ea904cb841b4a289edbde8073692278657c179eb
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-05-24 11:35:55 -0500

    add BranchingTechAcademy

---
+++ BranchingTechAcademy/BranchingTechAcademy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxDimensions = 50;
+            int maxWeight = 50;
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter a weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight <= maxWeight)
+            {
+                Console.WriteLine("Please enter the package width:");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height:");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
+                int length = Convert.ToInt32(Console.ReadLine());
+                int userDimensions = width + height + length;
+
+                if (userDimensions <= maxDimensions)
+                {
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + userDimensions * weight / 100.00);
+                    Console.WriteLine("Thank you.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
