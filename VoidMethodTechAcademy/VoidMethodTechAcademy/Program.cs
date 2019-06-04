ed098c2d3d8c2e99909fca735747f4d668e91808
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-06-03 20:45:25 -0500

    Add VoidMethodTechAcademy

---
+++ VoidMethodTechAcademy/VoidMethodTechAcademy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidTech voidTech = new VoidTech();
+            voidTech.Int1 = 4;
+            voidTech.Int2 = 2;
+            Console.WriteLine(voidTech.Int1 + " " + voidTech.Int2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethodTechAcademy/VoidMethodTechAcademy/VoidTech.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodTechAcademy
+{
+    class VoidTech
+    {
+        public void Sample()
+        {
+            Int1 = 4;
+            Int2 = 2;
+        }
+        public int Int1 { get; set; }
+        public int Int2 { get; set; }
+    }
+}
