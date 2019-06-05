bd4d5c67c5e49870bba7da4c4a68ed5660774bec
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-06-05 14:38:00 -0500

    Edit ForLoopTechAcademy

--- ForLoopTechAcademy/ForLoopTechAcademy/Program.cs
+++ ForLoopTechAcademy/ForLoopTechAcademy/Program.cs
@@ -10,14 +10,44 @@ namespace ForLoopTechAcademy
     {
         static void Main(string[] args)
         {
-            // Activity 1 - 5
-            //string []loopString = { "boop ", "bop "};
+            // Activity 2 
+            //int x = 1;
+            //while(x == 1)
+            //{
+            //    Console.WriteLine("This is an infinite loop");
+            //}
+
+            // Activity 3
+            //int x = 1;
+            //while (x == 1)
+            //{
+            //    Console.WriteLine("This is no longer an infinite loop");
+            //    x = x + 1;
+            //}
+            //Console.ReadLine();
+
+            // Activity 1, 4
+            //string[] loopString = { "boop ", "bop " };
+            //for (int i = 0; i < loopString.Length; i++)
+            //{
+            //    Console.WriteLine("Please enter something to add to the array.");
+            //    string userInput = Console.ReadLine();
+            //    Console.WriteLine(loopString[i]);
+            //    loopString[i] = loopString[i] + userInput;
+            //    Console.WriteLine(loopString[i]);
+            //}
+            //Console.ReadLine();
+
+
+            // Activity 5
+            //string[] loopString = { "boop ", "bop " };
             //for (int i = 0; i <= loopString.Length; i++)
             //{
             //    Console.WriteLine("Please enter something to add to the array.");
             //    string userInput = Console.ReadLine();
             //    Console.WriteLine(loopString[i]);
             //    loopString[i] = loopString[i] + userInput;
+            //    Console.WriteLine(loopString[i]);
             //}
             //Console.ReadLine();
 
@@ -31,23 +61,51 @@ namespace ForLoopTechAcademy
             //loopStringList.Add("strings");
             //Console.WriteLine("Insert either: 'This', 'is', 'a', 'list', 'of', or 'strings' ");
             //string stringInput = (Console.ReadLine());
-            //int index1 = loopStringList.IndexOf("This");
+            //string nope = ("The string you entered is not in the list.");
             //foreach (string loopString in loopStringList)
             //{
             //    if (stringInput == loopString)
             //    {
             //        Console.WriteLine(loopStringList.IndexOf(loopString));
+            //        Console.ReadLine();
+            //        Environment.Exit(0);
             //    }
             //    else
             //    {
-            //        Console.WriteLine("Enter a different string to see what its index is, and if you don't see a number then that means you need to input a string from the list.");
+            //        Console.WriteLine(nope);
             //    }
             //}
             //Console.ReadLine();
 
+            // Activity 9 - 10
+            //List<string> loopStringList = new List<string>();
+            //loopStringList.Add("this");
+            //loopStringList.Add("string");
+            //loopStringList.Add("is");
+            //loopStringList.Add("in");
+            //loopStringList.Add("this");
+            //loopStringList.Add("list");
+            //loopStringList.Add("of");
+            //loopStringList.Add("strings");
+            //Console.WriteLine("Insert either: 'this', 'string', 'is', 'in', 'this', 'list', 'of', or 'strings' ");
+            //string stringInput = (Console.ReadLine());
+            //for (var i = 0; i < loopStringList.Count; i++)
+            //{
+            //    if (stringInput == loopStringList[i])
+            //    {
+            //        Console.WriteLine(i);
 
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("You didn't enter a string that is in the list.");
+            //    }
+            //}
+            //Console.ReadLine();
+
+            // Activity 11
             List<string> loopStringList = new List<string>();
-            loopStringList.Add("This");
+            loopStringList.Add("this");
             loopStringList.Add("string");
             loopStringList.Add("is");
             loopStringList.Add("in");
@@ -55,18 +113,18 @@ namespace ForLoopTechAcademy
             loopStringList.Add("list");
             loopStringList.Add("of");
             loopStringList.Add("strings");
-            Console.WriteLine("Insert either: 'This', 'string', 'is', 'in', 'this', 'list', 'of', or 'strings' ");
+            Console.WriteLine("Insert either: 'this', 'string', 'is', 'in', 'this', 'list', 'of', or 'strings' ");
             string stringInput = (Console.ReadLine());
-            int index1 = loopStringList.IndexOf("This");
             foreach (string loopString in loopStringList)
             {
-                if (stringInput == loopString)
+                if (loopStringList.Contains(stringInput))
                 {
-                    Console.WriteLine(loopStringList.IndexOf(loopString));
+                    Console.WriteLine(loopStringList.IndexOf(stringInput));
+
                 }
                 else
                 {
-                    Console.WriteLine("Enter a different string to see what its index is, and if you don't see a number then that means you need to input a string from the list.");
+                    Console.WriteLine("You didn't enter a string that is in the list.");
                 }
             }
             Console.ReadLine();
