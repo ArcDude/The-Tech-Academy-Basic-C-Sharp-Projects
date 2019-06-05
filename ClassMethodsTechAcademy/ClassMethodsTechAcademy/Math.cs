2404cfa97752a5d42f60850ac55fd5559ac8a210
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-06-05 17:26:42 -0500

    Edit ClassMethodsTechAcademy

--- ClassMethodsTechAcademy/ClassMethodsTechAcademy/Math.cs
+++ ClassMethodsTechAcademy/ClassMethodsTechAcademy/Math.cs
@@ -10,17 +10,30 @@ namespace ClassMethodsTechAcademy
     {
         public int Add(int Num2, int Num1)
         {
+            Console.WriteLine("Enter a number to add to 7:");
+            Num1 = Convert.ToInt32(Console.ReadLine());
             Num2 = 7;
+            Console.WriteLine(Num1 + " + " + Num2 + " equals:");
+            Console.WriteLine(Num2 + Num1);
             return Num2 + Num1;
         }
         public int Sub(int Num2, int Num1)
         {
+            Console.WriteLine("Enter a number to subtract from 7:");
+            Num1 = Convert.ToInt32(Console.ReadLine());
             Num2 = 7;
+            Console.WriteLine(Num2 + " - " + Num1 + " equals:");
+            Console.WriteLine(Num2 - Num1);
             return Num2 - Num1;
         }
         public int Multi(int Num2, int Num1)
         {
+
+            Console.WriteLine("Enter a number to multiply by 7:");
+            Num1 = Convert.ToInt32(Console.ReadLine());
             Num2 = 7;
+            Console.WriteLine(Num2 + " * " + Num1 + " equals:");
+            Console.WriteLine(Num2 * Num1);
             return Num2 * Num1;
         }
         public int Num2 { get; set; }
--- ClassMethodsTechAcademy/ClassMethodsTechAcademy/Program.cs
+++ ClassMethodsTechAcademy/ClassMethodsTechAcademy/Program.cs
@@ -11,23 +11,10 @@ namespace ClassMethodsTechAcademy
         static void Main(string[] args)
         {
             Math math = new Math();
-            Console.WriteLine("What would you like to add to 7?");
-            math.Num1 = Convert.ToInt32(Console.ReadLine());
-            int sum = math.Num1 + math.Num2;
-            Console.WriteLine( math.Num1 + " plus " + math.Num2 + " equals " + sum);
+            int sum = math.Add(math.Num2, math.Num1);
+            int diff = math.Sub(math.Num2, math.Num1);
+            int multi = math.Multi(math.Num2, math.Num1);
             Console.ReadLine();
-
-            //Console.WriteLine("What would you like to subtract with 7?");
-            //math.Num1 = Convert.ToInt32(Console.ReadLine());
-            //int difference = math.Num1 - math.Num2;
-            //Console.WriteLine(math.Num1+ " minus " + math.Num2 + " equals " + difference);
-            //Console.ReadLine();
-
-            //Console.WriteLine("What would you like to multiply with 7?");
-            //math.Num1 = Convert.ToInt32(Console.ReadLine());
-            //int product = math.Num1 * math.Num2;
-            //Console.WriteLine(math.Num1 + " times " + math.Num2 + " equals " + product);
-            //Console.ReadLine();
         }
     }
 }
