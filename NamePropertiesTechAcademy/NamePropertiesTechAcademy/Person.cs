105552e976077c8c81c193af71a11106bbbd5e72
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-06-10 12:43:44 -0500

    Add NamePropertiesTechAcademy

---
+++ NamePropertiesTechAcademy/NamePropertiesTechAcademy/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamePropertiesTechAcademy
+{
+    public class Person
+    {
+        public void SayName()
+        {
+            FirstName = "Sample";
+            LastName = "Student";
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ NamePropertiesTechAcademy/NamePropertiesTechAcademy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamePropertiesTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.SayName();
+            employee.Identifcation();
+            Console.ReadLine();
+        }
+    }
+}
