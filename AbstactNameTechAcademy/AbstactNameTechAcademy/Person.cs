53b0a8cec6b0cbeadf4c5489d1bd5c9e8bd7a203
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-06-10 13:43:58 -0500

    Add AbstractNameTechAcademy

---
+++ AbstactNameTechAcademy/AbstactNameTechAcademy/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstactNameTechAcademy
+{
+   public abstract class Person
+    {
+        public virtual void SayName()
+        {
+            FirstName = "Sample";
+            LastName = "Student";
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
