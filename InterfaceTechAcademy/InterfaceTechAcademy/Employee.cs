4c36d4946937ee8ae53062add9df8c4e2604c6b3
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-06-12 13:39:14 -0500

    Add InterfaceTechAcademy

---
+++ InterfaceTechAcademy/InterfaceTechAcademy/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTechAcademy
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+        public void Quit(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
