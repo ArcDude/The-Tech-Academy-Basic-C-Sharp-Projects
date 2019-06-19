9533716c7e94173d2262fc7c9df38e5fba0716f1
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-06-19 18:50:16 -0500

    Add WriteTextTechAcademy

---
+++ LambdaTechAcademy/LambdaTechAcademy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaTechAcademy
+{
+    class Program
+    {
+        enum Employee
+        {
+            Joe_Bruns,
+            Adam_Smith,
+            Joe_Baily,
+            Alexa_Hays,
+            Alisa_Max,
+            Edith_Mays,
+            Josh_Perterson,
+            Ashley_Brown,
+            Barton_Spring,
+            Susan_Pho
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (string person in Enum.GetNames(typeof(Employee)))
+            {
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ WriteTextTechAcademy/WriteTextTechAcademy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteTextTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered {0} in your number.txt file on your desktop.", number);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Schui\Desktop\number.txt", true))
+            {
+                file.WriteLine(number);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
