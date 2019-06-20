9c83339e1108b3626318687f701a36d54e427826
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-06-19 19:14:40 -0500

    Add DateTimeTechAcademy

---
+++ DateTimeTechAcademy/DateTimeTechAcademy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number:");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime.Now.AddHours(hours);
+            Console.WriteLine("It will be {0} in {1} hour(s)", DateTime.Now.AddHours(hours), hours);
+            Console.ReadLine();
+
+        }
+    }
+}
