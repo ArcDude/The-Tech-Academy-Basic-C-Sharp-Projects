048590891a742823852f68e158e6cd7ab89a416b
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-06-20 17:56:39 -0500

    Add ConstTechAcademy

---
+++ ConstTechAcademy/ConstTechAcademy/Chaining.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstTechAcademy
+{
+    public class Chaining
+    {
+        string add = "This string is being added to the chain.";
+        int addint = 4;
+        public Chaining(string chain1) : this(chain1, 12)
+        {
+        }
+        public Chaining(string chain1, int chain2)
+        {
+            add = chain1;
+            addint = chain2;
+        }
+       
+    }
+}
---
+++ ConstTechAcademy/ConstTechAcademy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string sample = "This is a string as a constant variable.";
+            var example = "This is an example of a string with the keyword 'var'.";
+            Console.WriteLine("Constant: {0} Variable: {1}", sample, example);
+
+
+
+        }
+    }
+}
