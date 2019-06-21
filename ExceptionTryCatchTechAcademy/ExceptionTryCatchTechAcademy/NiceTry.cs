4f3bc9663656e541898f46b9bb4c031605bb9b7d
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-06-20 19:58:41 -0500

    Add ExceptionTryCatchTechAcademy

---
+++ ExceptionTryCatchTechAcademy/ExceptionTryCatchTechAcademy/NiceTry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTryCatchTechAcademy
+{
+    public class NiceTry : Exception
+    {
+        public NiceTry()
+            : base() { }
+        public NiceTry(string message)
+            : base(message) { }
+    }
+}
---
+++ ExceptionTryCatchTechAcademy/ExceptionTryCatchTechAcademy/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTryCatchTechAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool validAnswer = false;
+            int age = 0;
+            while (!validAnswer)
+            {
+                    Console.WriteLine("What is your age?");
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer) Console.WriteLine("Please enter an integer.");
+            }
+            if (age <= 0)
+            {
+                throw new NiceTry();
+            }
+            bool ageTest = true;
+            while (ageTest)
+            {
+                try
+                {
+                    Console.WriteLine("You were born in the year of {0}", DateTime.Now.Year - age);
+                    Console.ReadLine();
+                    return;
+                }
+                catch (NiceTry)
+                {
+                    Console.WriteLine("Nope. Can't fool this program.");
+                    Console.ReadLine();
+                    return;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You broke it.");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
